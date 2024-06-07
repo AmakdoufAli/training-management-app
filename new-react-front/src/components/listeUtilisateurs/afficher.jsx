35bdb90619b630de1e7e5ffcd57a7926777eb724
ALI <aliamakdouf@gmail.com>
2024-06-07 12:31:52 +0100

    update some changes

--- new-react-front/src/components/listeUtilisateurs/afficher.jsx
+++ new-react-front/src/components/listeUtilisateurs/afficher.jsx
@@ -1,72 +1,131 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-import axios from "axios"
-import { Link } from "react-router-dom"
+export default function Afficher(props) {
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [deleteUserId, setDeleteUserId] = useState(null);
 
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/users/${id}`);
+      props.refetch();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-export default function Afficher(props){
+  const handleDeleteClick = (id) => {
+    setDeleteUserId(id);
+    setShowConfirm(true);
+  };
 
-    const deleteUser = async(id) =>{
-        if(window.confirm('Vouler-vous vraiment supprimer l\'utilisateur avec id '+ id + ' ?')){
-            try{
-                await axios.delete(`http://127.0.0.1:8000/api/users/${id}`);
-                props.refetch();
-            }catch(err){
-                console.error(err);
-            }
-        }
-    } 
+  const handleConfirmDelete = () => {
+    if (deleteUserId) {
+      deleteUser(deleteUserId);
+    }
+    setShowConfirm(false);
+    setDeleteUserId(null);
+  };
 
-    return (
-        <div className="bg-white mb-32">
-            <header>
-            <div className="m-0 mb-8">
-                <div style={{backgroundColor: "#F5A962"}} className=" overflow-hidden w-full shadow-sm  rounded-t-lg">
-                    <div className="p-3 text-gray-900 flex justify-between">
-                        
-                        <div className="flex justify-start items-center ">
-                            <h1 className="text-[20px] ml-2 text-white  mr-5">Liste des utilisateurs</h1>
-                        </div>      
-                    </div>
-                </div>
+  const handleCancelDelete = () => {
+    setShowConfirm(false);
+    setDeleteUserId(null);
+  };
+
+  return (
+    <div className="bg-white mb-32">
+      <header>
+        <div className="m-0 mb-8">
+          <div
+            style={{ backgroundColor: "#F5A962" }}
+            className="overflow-hidden w-full shadow-sm rounded-t-lg"
+          >
+            <div className="p-3 text-gray-900 flex justify-between">
+              <div className="flex justify-start items-center ">
+                <h1 className="text-[20px] ml-2 text-white mr-5">Liste des utilisateurs</h1>
+              </div>
             </div>
-            </header>    
-
-            
-            {/* boutton d'ajout */}
-            <div className="mx-3 my-6">
-              <Link to={"/ajouterUser"} className="py-2 px-6 text-md font-medium text-center text-white rounded-lg bg-blue-4">Ajouter Utilisateur</Link>
-            </div>
-
-            {/* tableau d'affichage */}
-            <div >
-                <table className='text-sm text-left rtl:text-right text-gray-500 w-full'>
-                    <thead className='border-b text-md text-left text-gray-700 uppercase bg-gray-50'>
-                        <tr>
-                        <th className='px-6 py-3'>id</th>
-                        <th className='px-6 py-3'>Name</th>
-                        <th className='px-6 py-3'>Email</th>
-                        <th className='px-6 py-3'>Role</th>
-                        <th className='px-6 py-3'>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.users && props.users.map((user)=>(
-                        <tr key={user.id} className='odd:text-black odd:dark:bg-gray-100 even:bg-gray-50  border-b'>
-                            <td className='px-6 py-3'>{user.id}</td>
-                            <td className='px-6 py-3'>{user.name}</td>
-                            <td className='px-6 py-3'>{user.email}</td>
-                            <td className='px-6 py-3'>{user.role}</td>
-                            <td className='px-6 py-3 flex'>
-                                <Link to={`/modifierUser/${user.id}`} className='mr-2 rounded text-orange-950 bg-orange-200' title="modifier"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"> <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg></Link>
-                                <button onClick={()=>deleteUser(user.id)} className='text-red-800 bg-red-300 mr-2 rounded' title="supprimer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg></button>
-                                {/* <a href={`modifier/${user.id}`} className='text-red-800 bg-red-300 mr-2 rounded' title="supprimer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg></a> */}
-                            </td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            
+          </div>
         </div>
-    )
+      </header>
+
+      {/* boutton d'ajout */}
+      <div className="mx-3 my-6">
+        <Link to={"/ajouterUser"} className="py-2 px-6 text-md font-medium text-center text-white rounded-lg bg-blue-4">
+          Ajouter Utilisateur
+        </Link>
+      </div>
+
+      {/* tableau d'affichage */}
+      <div>
+        <table className="text-sm text-left rtl:text-right text-gray-500 w-full">
+          <thead className="border-b text-md text-left text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th className="px-6 py-3">id</th>
+              <th className="px-6 py-3">Name</th>
+              <th className="px-6 py-3">Email</th>
+              <th className="px-6 py-3">Role</th>
+              <th className="px-6 py-3">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.users &&
+              props.users.map((user) => (
+                <tr key={user.id} className="odd:text-black odd:dark:bg-gray-100 even:bg-gray-50 border-b">
+                  <td className="px-6 py-3">{user.id}</td>
+                  <td className="px-6 py-3">{user.name}</td>
+                  <td className="px-6 py-3">{user.email}</td>
+                  <td className="px-6 py-3">{user.role}</td>
+                  <td className="px-6 py-3 flex">
+                    <Link to={`/modifierUser/${user.id}`} className="mr-2 rounded text-orange-950 bg-orange-200" title="modifier">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                        />
+                      </svg>
+                    </Link>
+                    <button onClick={() => handleDeleteClick(user.id)} className="text-red-800 bg-red-300 mr-2 rounded" title="supprimer">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Confirmation Dialog */}
+      {showConfirm && (
+        <div className="modal-overlay fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+          <div className="modal bg-white rounded-lg p-4 shadow-lg">
+            <h2 className="text-xl font-bold mb-4">Confirm Delete</h2>
+            <p className="mb-4">Êtes-vous sûr de vouloir supprimer ce utilisateur ?</p>
+            <div className="modal-buttons flex justify-end">
+              <button
+                className="cancel-button mr-2 px-4 py-2 bg-gray-300 text-gray-700 rounded"
+                onClick={handleCancelDelete}
+              >
+                Cancel
+              </button>
+              <button
+                className="delete-button px-4 py-2 bg-red-600 text-white rounded"
+                onClick={handleConfirmDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- new-react-front/src/pages/brouillons/Brouillons .jsx
+++ new-react-front/src/pages/brouillons/Brouillons .jsx
@@ -1,131 +1,104 @@
-import React, { useEffect } from 'react';
-// import './Brouillons.css'; 
+import React, { useEffect, useState } from 'react';
 import { useGetBrouillonsQuery } from '../../features/apiSlice';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Layout from '../../components/template/Layout';
 
 function Brouillons() {
-
     const navigate = useNavigate();
+    const { data: brouillons } = useGetBrouillonsQuery();
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [deleteBrouillonId, setDeleteBrouillonId] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         !localStorage.getItem('user') && navigate('/login');
-    },[]);
+    }, []);
 
-    const { data:brouillons } = useGetBrouillonsQuery();
-
-    const handleDelete = (id) => {
-        alert("Suppression du brouillon avec l'ID :", id);
-    }
-
-    const publish = async (id) => {
-        if (window.confirm("Êtes-vous sûr de publier cette formation ?")) {
-            try {
-                const response = await axios.put(`http://127.0.0.1:8000/api/brouillon-publish/${id}`);
-                console.log(response);
-            } catch (error) {
-                console.log(error);
-            }
-        }
+    const handleDeleteClick = (id) => {
+        setDeleteBrouillonId(id);
+        setShowConfirm(true);
     };
 
-    const deleteBrouillons = async(id) =>{
-        if(window.confirm('Vouler-vous vraiment supprimer cette brouillon numéro '+ id + ' ?')){
-            try{
-                await axios.delete(`http://127.0.0.1:8000/api/documentationByIdFormation/${id}`);
-                await axios.delete(`http://127.0.0.1:8000/api/notificationByIdFormation/${id}`);
-                await axios.get(`http://127.0.0.1:8000/api/detaching/${id}`);
-                await axios.delete(`http://127.0.0.1:8000/api/formations/${id}`);
-            }catch(err){
-                alert('On peut pas supprimer cette à cause d\'un probleme non reconu !')
+    const handleConfirmDelete = async () => {
+        if (deleteBrouillonId) {
+            try {
+                await axios.delete(`http://127.0.0.1:8000/api/documentationByIdFormation/${deleteBrouillonId}`);
+                await axios.delete(`http://127.0.0.1:8000/api/notificationByIdFormation/${deleteBrouillonId}`);
+                await axios.get(`http://127.0.0.1:8000/api/detaching/${deleteBrouillonId}`);
+                await axios.delete(`http://127.0.0.1:8000/api/formations/${deleteBrouillonId}`);
+                // Trigger refetch or update the state to remove the deleted item from the list
+            } catch (err) {
+                alert("On ne peut pas supprimer cette brouillon à cause d'un problème non reconnu !");
             }
         }
-    }
+        setShowConfirm(false);
+        setDeleteBrouillonId(null);
+    };
+
+    const handleCancelDelete = () => {
+        setShowConfirm(false);
+        setDeleteBrouillonId(null);
+    };
 
     return (
         <Layout>
             <div>
                 <header>
                     <div className="m-0 mb-8">
-                        <div style={{backgroundColor: "#F5A962"}} className=" overflow-hidden w-full shadow-sm  rounded-t-lg">
+                        <div style={{ backgroundColor: "#F5A962" }} className="overflow-hidden w-full shadow-sm rounded-t-lg">
                             <div className="p-3 text-gray-900 flex justify-between">
-                                
-                                <div className="flex justify-start items-center ">
-                                    <h1 className="text-[20px] ml-2 text-white  mr-5">Liste Des Brouillons</h1>
-                                </div>      
+                                <div className="flex justify-start items-center">
+                                    <h1 className="text-[20px] ml-2 text-white mr-5">Liste Des Brouillons</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </header>    
+                </header>
                 {brouillons && brouillons.map((formation, index) => (
-
                     <div className="p-5 space-y-3" key={index}>
-                        <div className='text-center py-1 px-5 font-semibold bg-gray-1 rounded-lg w-fit'>
+                        <div className="text-center py-1 px-5 font-semibold bg-gray-1 rounded-lg w-fit">
                             <p>Formation</p>
                             <p>n°{formation.id}</p>
                         </div>
                         <div className="flex items-center justify-between">
-                            <div className='flex items-center gap-x-8 text-lg'>
+                            <div className="flex items-center gap-x-8 text-lg">
                                 <p><b>Titre : </b>{formation.titre}</p>
                                 <p> | {formation.date_debut}</p>
                             </div>
                             <div className="flex items-center gap-3 text-lg">
-                                <Link to={`/brouillons/edit/${formation.id}`} className='bg-orange-1 py-1 rounded-lg text-white px-4'>Modifier</Link>
-                                <button className='bg-orange-1 py-1 rounded-lg text-white px-4' onClick={()=>deleteBrouillons(formation.id)}>Supprimer</button>
+                                <Link to={`/brouillons/edit/${formation.id}`} className="bg-orange-1 py-1 rounded-lg text-white px-4">Modifier</Link>
+                                <button className="bg-orange-1 py-1 rounded-lg text-white px-4" onClick={() => handleDeleteClick(formation.id)}>Supprimer</button>
                             </div>
                         </div>
                         <hr />
                     </div>
-
                 ))}
             </div>
+
+            {showConfirm && (
+                <div className="modal-overlay fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+                    <div className="modal bg-white rounded-lg p-4 shadow-lg">
+                        <h2 className="text-xl font-bold mb-4">Confirmer la suppression</h2>
+                        <p className="mb-4">Voulez-vous vraiment supprimer cette brouillon numéro {deleteBrouillonId} ? Cette action est irréversible.</p>
+                        <div className="modal-buttons flex justify-end">
+                            <button
+                                className="cancel-button mr-2 px-4 py-2 bg-gray-300 text-gray-700 rounded"
+                                onClick={handleCancelDelete}
+                            >
+                                Annuler
+                            </button>
+                            <button
+                                className="delete-button px-4 py-2 bg-red-600 text-white rounded"
+                                onClick={handleConfirmDelete}
+                            >
+                                Supprimer
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </Layout>
     );
-
 }
 
 export default Brouillons;
-
-
-    
-
-    // return (
-    //     <Layout>
-    //         <div>
-    //             <h2 className='text-3xl font-bold m-3'>Liste de Brouillons</h2>
-    //             <table className="text-sm text-left rtl:text-right text-gray-500 w-full">
-    //                 <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
-    //                     <tr>
-    //                         <th className='px-6 py-3'>ID</th>
-    //                         <th className='px-6 py-3'>Titre</th>
-    //                         <th className='px-6 py-3'>Nombre d'heures</th>
-    //                         <th className='px-6 py-3'>Nombre de jours</th>
-    //                         <th className='px-6 py-3'>Date de début</th>
-    //                         <th className='px-6 py-3'>Date de fin</th>
-    //                         <th className='px-6 py-3'>Adresse</th>
-    //                         <th className='px-6 py-3'>Actions</th>
-    //                     </tr>
-    //                 </thead>
-    //                 <tbody>
-    //                     {brouillons && brouillons.map((brouillon, index) => (
-    //                         <tr key={index} className='odd:text-black odd:dark:bg-gray-100 even:bg-gray-50'>
-    //                             <td className='px-6 py-3'>{brouillon.id}</td>
-    //                             <td className='px-6 py-3'>{brouillon.titre}</td>
-    //                             <td className='px-6 py-3'>{brouillon.nbr_heures}</td>
-    //                             <td className='px-6 py-3'>{brouillon.nbr_jours}</td>
-    //                             <td className='px-6 py-3'>{brouillon.date_debut}</td>
-    //                             <td className='px-6 py-3'>{brouillon.date_fin}</td>
-    //                             <td className='px-6 py-3'>{brouillon.adresse}</td>
-    //                             <td className='px-6 py-3 flex items-center gap-2'>
-    //                                 <Link to={`/brouillons/edit/${brouillon.id}`} className='bg-blue-500 p-3 rounded-md text-white font-semibold'>Modifier</Link>
-    //                                 <button onClick={() => handleDelete(brouillon.id)} className='bg-red-500 p-3 rounded-md text-white font-semibold'>Supprimer</button>
-    //                                 {/* <button onClick={() =>publish(brouillon.id)} className='bg-green-500 p-3 rounded-md text-white font-semibold'>Publish</button> */}
-    //                             </td>
-    //                         </tr>
-    //                     ))}
-    //                 </tbody>
-    //             </table>
-    //         </div>
-    //     </Layout>
-    // );
